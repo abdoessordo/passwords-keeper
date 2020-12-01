0cc636824c037ee5b0a03fe581e4e4ca39d6592f
Essordo Abdellah <62575309+abdoessordo@users.noreply.github.com>
2020-12-01 14:45:27 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 import json
-from send_email import send_email_for_new_users
+from send_email import send_email_for_new_users, send_email_new_password_added
 
-DATA = {}
+USERS_DATA = {}
 USERS = []
 
 
-def load_DATA():
-	global DATA
-	if os.path.isfile("data.json"):
-		with open("data.json", "r") as f:
-			DATA = json.load(f)
+def load_DATA(file):
+	if os.path.isfile(file):
+		with open(file, "r") as f:
+			return json.load(f)
+
 
 
 def get_Users(dicti):
@@ -18,14 +18,6 @@ def get_Users(dicti):
 	for key in dicti.keys():
 			users.append(key)
 	return users
-
-	
-def get_usernames(dicti):
-	users = get_Users(dicti)
-	usernaems = []
-	for user in users:
-		usernaems.append(dicti[user]["username"])
-	return usernaems
 
 
 def get_emails(dicti):
@@ -37,72 +29,110 @@ def get_emails(dicti):
 
 
 def add_user(firstname, lastname, username, email, password):
-	if username in(get_usernames(DATA)) or email in(get_emails(DATA)):
+	global USERS
+
+	if (username in USERS) or email in(get_emails(USERS_DATA)):
 		print("Username or Email is already taken please enter another one.")
 	else:
-		DATA[f"{firstname} {lastname}"] = {
-											"firstname" : firstname,
-											"lastname" : lastname,
-											"username" : username,
-											"email" : email,
-											"password" : password
-										}
+		USERS_DATA[username] = {
+			"firstname" : firstname,
+			"lastname" : lastname,
+			"username" : username,
+			"email" : email,	
+			"password" : password
+		}
 		send_email_for_new_users(firstname, email)
-		print(f"Welcome {firstname}! We garantee you that all your passwords are in safe hands :) .")
+		print(f"{firstname} {lastname} have been added")
 
-		global USERS
-		USERS = get_Users(DATA)
+		with open(f'./passwords_DATA/{username}_passwords.json', 'w') as f:
+			json.dump({}, f, indent=4)
+
+		print(USERS_DATA)
+		USERS = get_Users(USERS_DATA)
 
 
-def clear_DATA(mode=all):
-	global DATA
+def clear_USERS_DATA(mode):
+	global USERS_DATA
 
 	if mode == all:
-		DATA = {}
-		get_Users(DATA)
-		return DATA
+		USERS_DATA = {}
+		for file in os.listdir('passwords_DATA'):
+			os.remove(f'./passwords_DATA/{file}')
+		print("All users have been deleted")
+		return USERS_DATA
 
-	elif mode in(get_Users(DATA)):
-		get_Users(DATA)
-		DATA.pop(mode)
-		return DATA
+	elif mode in(get_Users(USERS_DATA)):
+		USERS_DATA.pop(mode)
+		os.remove(f'./passwords_DATA/{mode}_passwords.json')
+		print(f"{mode} have been removed")
+		return USERS_DATA
+
+	else: print("User you selected can't be found")
 
 
 def login(username, password):
-	if username not in(get_usernames(DATA)):
+	if username not in(get_usernames(USERS_DATA)):
 		print("Username not valid.")
 		return False
 	else:
-
-
-
-		for user in get_Users(DATA):
-			if DATA[user]["username"] == username and DATA[user]["password"] == password:
-				print("loged in succesfully")
-				return True
-			elif DATA[user]["username"] == username and DATA[user]["password"] != password: 
-				print("Password incorrect. Try again.")
+		for user in get_Users(USERS_DATA):
+			if USERS_DATA[user]["username"] == username and USERS_DATA[user]["password"] == password:
+				return USERS_DATA[user]["email"]
+			elif USERS_DATA[user]["username"] == username and USERS_DATA[user]["password"] != password: 
 				return False
 
-				
 
-load_DATA()
+def add_password(username, app, link, password):
+	temp = load_passwords(username)
+	if app in list(load_passwords(username).keys()):
+		print("A password for this app have already been added to your account.")
+	else:
+		temp[app] = {
+			"appname" : app,
+			"link" : link,
+			"password" : password
+		}
+		
+		send_email_new_password_added(USERS_DATA[username]["firstname"], app, USERS_DATA[username]["email"])
 
-# login("abdoessordo", "Tanger.2001")
+		with open(f"./passwords_DATA/{username}_passwords.json", "w") as f:
+			json.dump(temp, f, indent=4)	
+
+def load_passwords(username):
+	return load_DATA(f"./passwords_DATA/{username}_passwords.json")
+
+
+
+USERS_DATA = load_DATA("users_data.json")
+
+
+
+
+
+
+
+
+
+# add_password("abdoessordo", "facebook", "facebook.com", "Tanger.2001")
+add_password("abdoessordo", "gmail", "gmail.com", "Tanger.2001")
+
+print(list(load_passwords("abdoessordo").keys()))
+
+
+# if login("abdoessordo", "Tanger.2001"): print("logged in succesfully,", login("abdoessordo", "Tanger.2001"))
+
 
 # add_user("Abdellah", "Essordo", "abdoessordo", "abdoessordo01@gmail.com", "Tanger.2001")
-# add_user("firstname", "lastname", "username", "email", "password")
-# add_user("Ayman", "Essordo", "ayman272002", "ayman2272002@gmail.com", "Tanger.2002")
+# add_user("Ayman", "Essordo", "ayman2272002", "ayman2272002@gmail.com", "Tanger.2002")
 
 
-# clear_DATA("Ayman Essordo")
-# clear_DATA()
+# clear_USERS_DATA("ayman2272002")
+# clear_USERS_DATA(all)
 
-print("DATA :", DATA, "\n")
-# print("USERS :", get_Users(DATA))
-# print("USERNAMES :", get_usernames(DATA))
-# print("EMAILS :", get_emails(DATA), '\n')
+# print("USERS :", get_Users(USERS_DATA))
+# print("USERNAMES :", get_usernames(USERS_DATA))
+# print("EMAILS :", get_emails(USERS_DATA))
 
 
-with open("data.json", "w") as f:
-	json.dump(DATA, f, indent=4)
+with open("users_data.json", "w") as f:
+	json.dump(USERS_DATA, f, indent=4)
--- send_email.py
+++ send_email.py
@@ -12,22 +12,28 @@ def send_email_for_new_users(name, user_email):
 	msg['Subject'] = 'Welcome on board.'
 	msg['From'] = EMAIL_ADRESS
 	msg['To'] = user_email
-	msg.set_content(f'''
-Welcome {name}!
-We are glad to have you with on board and to put your trust in us. We guarantee you that all your passwords are in safe hands :) .
-
-For any help, contact us on this email, and we'll do our best to help you. 
-
-
-EA corp.
-					'''
-					)
+	msg.set_content(
+		f"Welcome {name}!\nWe are glad to have you with on board and to put your trust in us. We guarantee you that all your passwords are in safe hands :) .\nFor any help, contact us on this email, and we'll do our best to help you.\n\nEA corp."
+		)
 
 	with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 		smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
 		smtp.send_message(msg)
 
 
+def send_email_new_password_added(name, app, user_email):
+	global EMAIL_ADRESS, EMAIL_PASSWORD
+	msg = EmailMessage()
+	msg['Subject'] = 'New password added to your account.'
+	msg['From'] = EMAIL_ADRESS
+	msg['To'] = user_email
+	msg.set_content(
+		f"Hi {name},\nYou have successfully added a password for your {app} account. \nFor any help, contact us on this email, and we'll do our best to help you.\n\nEA corp."
+		)
+
+	with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+		smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
+		smtp.send_message(msg)
 
 
 
